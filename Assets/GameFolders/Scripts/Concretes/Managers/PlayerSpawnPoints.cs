abdb4c4514536f635021a6d5786878a333a6e210
oguzkaansarikaya <oguzkaansarikaya@hotmail.com>
2023-04-08 19:04:03 +0300

    Player Position Fixed

--- Assets/GameFolders/Scripts/Concretes/Managers/PlayerSpawnPoints.cs
+++ Assets/GameFolders/Scripts/Concretes/Managers/PlayerSpawnPoints.cs
@@ -7,7 +7,7 @@ public class PlayerSpawnPoints : MonoBehaviour
     [SerializeField] GameObject spawn_point_ref;
     void Start()
     {
-        
+        transform.position = new Vector3(spawn_point_ref.transform.position.x, spawn_point_ref.transform.position.y,0f);
     }
 
     // Update is called once per frame
