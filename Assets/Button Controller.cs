e8afa39cb9f87c373039d687b675bab4ab4359f4
Hasan Efe Dinç <efedinc35@hotmail.com>
2023-04-09 01:48:21 +0300

    Main Menu UI Buttons

---
+++ Assets/Button Controller.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    private bool questionAnswered;
+    [SerializeField] private GameObject Closet;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
