2a4fdb8c152f90f7d607c01fe97e8706f2ab1a54
Burak Sevincli <brksvncli@gmail.com>
2023-04-09 19:57:48 +0300

    GameScene Texts Completed

--- Assets/GameFolders/Scripts/Concretes/Managers/ButtonManagement.cs
+++ Assets/GameFolders/Scripts/Concretes/Managers/ButtonManagement.cs
@@ -6,6 +6,7 @@ public class ButtonManagement : MonoBehaviour
 {
     public bool Question_controller;
 
+    [SerializeField] private GameObject button;
     public void questionController()
     {
         Question_controller = true;
