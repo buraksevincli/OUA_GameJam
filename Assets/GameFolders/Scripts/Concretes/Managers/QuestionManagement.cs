9885f3a132fbd3c966c4cf38b5e8cef8128dbcbd
oguzkaansarikaya <oguzkaansarikaya@hotmail.com>
2023-04-09 15:04:23 +0300

    Home enter is fixed

--- Assets/GameFolders/Scripts/Concretes/Managers/QuestionManagement.cs
+++ Assets/GameFolders/Scripts/Concretes/Managers/QuestionManagement.cs
@@ -19,6 +19,13 @@ public class QuestionManagement : MonoBehaviour
     [SerializeField] GameObject npc5;
     [SerializeField] GameObject npc6;
     [SerializeField] GameObject npc7;
+    [SerializeField] GameObject home1;
+    [SerializeField] GameObject home2;
+    [SerializeField] GameObject home3;
+    [SerializeField] GameObject home4;
+    [SerializeField] GameObject home5;
+    [SerializeField] GameObject home6;
+    [SerializeField] GameObject home7;
     void Start()
     {
         question1Bool = GameManager.Instance.Question1;
@@ -30,37 +37,44 @@ public class QuestionManagement : MonoBehaviour
         question7Bool = GameManager.Instance.Question7;
         if (question1Bool==true)
         {
-            Destroy(npc1.GetComponent<BoxCollider2D>()); 
+            Destroy(npc1.GetComponent<BoxCollider2D>());
+            home1.SetActive(true);
         }
 
         if (question2Bool == true)
         {
             Destroy(npc2.GetComponent<BoxCollider2D>());
+            home2.SetActive(true);
         }
 
         if (question3Bool == true)
         {
             Destroy(npc3.GetComponent<BoxCollider2D>());
+            home3.SetActive(true);
         }
 
         if (question4Bool == true)
         {
             Destroy(npc4.GetComponent<BoxCollider2D>());
+            home4.SetActive(true);
         }
 
         if (question5Bool == true)
         {
             Destroy(npc5.GetComponent<BoxCollider2D>());
+            home5.SetActive(true);
         }
 
         if (question6Bool == true)
         {
             Destroy(npc6.GetComponent<BoxCollider2D>());
+            home6.SetActive(true);
         }
 
         if (question7Bool == true)
         {
             Destroy(npc7.GetComponent<BoxCollider2D>());
+            home7.SetActive(true);
         }
     }
 }
